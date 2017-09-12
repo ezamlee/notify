ded0e91c1b0ffd957fb4f82995c56387a0472166
ezamlee <a.m.essam1992@gmail.com>
2017-09-12 11:29:24 +0200

    factory barebone

--- notify.js
+++ notify.js
@@ -2,10 +2,75 @@
 //requrie modules here
 
 module.exports = {
-	factory : function(){
-		console.log("I am a factory")
+	factory : function(topic,listnerProtocol,publisherProtocols){ 		//listnerProtocol => ['ws','rest'], publisherProtocols => ['ws','rest']
+				// each protocol server implementation
+			var server{
+				ws:{
+					listner     : function(config){
+						//return server
+					},store
+					publisher   : function(config){
+						//return server
+					},
+					addLChannel : function(config){
+						//return server
+					},
+					addPChannel : function(config){
+						//return server
+					}
+				},
+				rest:{
+					listner     : function(config){
+						//return server
+					},
+					publisher   : function(config){
+						//return server
+					},
+					addLChannel : function(config){
+						//return server
+					},
+					addPChannel : function(config){
+						//return server
+					}
+				}
+			}
+			var initListeners = (listnerProtocol)=>{
+				listnerProtocol.foreach((protocol)=>{
+					server[protocol].listner(
+						{
+							//enter configuration here
+						}
+					)
+					server[protocol].addLChannel(
+						{
+							//enter configuration here
+						}
+					)
+				})
+			}
+			var initPublishers = (publisherProtocols)=>{
+				publisherProtocols.foreach((protocol)=>{
+					this.server[protocol].publisher(
+						{
+							//enter configuration here
+						}
+					)
+					this.server[protocol].addPChannel(
+						{
+							//enter configuration here
+						}
+					)
+				})
+			}
 	},
-	list : function(){
-		console.log("I am the list");
+	list : {
+		store :{
+			"DemoChannel" : []
+		}
+	},
+	servers : {
+		ws:null,
+		mqtt:null,
+		rest:null
 	}
 }
