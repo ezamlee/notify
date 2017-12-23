e17cbfdcc7c35398ff62c8cce7c45c62c07bb09b
ezamlee <a.m.essam1992@gmail.com>
2017-12-23 08:36:18 +0200

    fix server bug

--- publicApiServer/routers/secure.js
+++ publicApiServer/routers/secure.js
@@ -36,9 +36,10 @@ router.post("/notification/:childTag/:page/:skip", function (req, resp) {
     Promise.resolve(validator.isInt(tagID))
         .then((tagIsValed) => {
             if (tagIsValed) {
+                console.log(req.decoded);
                 if (req.decoded.data[0].children[tagID]) {
                     req.decoded.data[0].children[tagID]["id"] = tagID;
-                    return req.decoded.children[tagID]
+                    return req.decoded.data[0].children[tagID]
                 } else {
                     throw "cannot find the child record"
                 }
