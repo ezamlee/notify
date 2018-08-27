1af23ebecbf061ab5d9ade6cca985175bfce07ab
ezamlee <a.m.essam1992@gmail.com>
2018-08-27 21:26:40 +0200

    updated session

--- conf/serverconf.js
+++ conf/serverconf.js
@@ -1,8 +1,8 @@
 module.exports = {
-	"token" : "0e31585320d29e3db8ca8cbeab99ed5f865DECF10BCA226E59E53EAD37438D898F0C5257",
+	"token"      : "0e31585320d29e3db8ca8cbeab99ed5f865DECF10BCA226E59E53EAD37438D898F0C5257",
 	"secretWord" : "mysecretword",
 	"source"     : "15766062",
 	"mailPass"   : "P@$$w0rd123",
 	"mailUser"   : "schooltrackingsystems@gmail.com",
-	"cycle"			 : "dev"
+	"cycle"		 : "dev"
 }
