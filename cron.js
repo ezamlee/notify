3abf08e5003061783ae7dc020f491dbbb54662c4
ezamlee <a.m.essam1992@gmail.com>
2018-08-27 22:28:29 +0200

    mase

--- cron.js
+++ cron.js
@@ -7,16 +7,16 @@ var rp = require('request-promise');
 var conf = require('./publicApiServer/conf/serverconf');
 function update_data() {
 	rp({
-			uri: `https://hst-api.wialon.com/wialon/ajax.html?svc=token/login&params={"token":"0e31585320d29e3db8ca8cbeab99ed5f8FF39C43F147343E1164472619A14ADA69B535B7"}`,
-			method: "GET",
-			header: {
-				"Content-Type": "application/json",
-				"Accept": "application/json"
-			},
-			json: true
-		})
+		uri: `https://hst-api.wialon.com/wialon/ajax.html?svc=token/login&params={"token":"0e31585320d29e3db8ca8cbeab99ed5f8FF39C43F147343E1164472619A14ADA69B535B7"}`,
+		method: "GET",
+		header: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		json: true
+	})
 		.then((wialonResp) => {
-			console.log(wialonResp,wialonResp.eid)
+			console.log(wialonResp, wialonResp.eid)
 			return wialonResp.eid
 		})
 		.then((eid) => {
@@ -32,27 +32,30 @@ function update_data() {
 		})
 		.then((resources) => {
 			console.log(resources);
-			if(resources && resources.items){
-				console.log(resources,resources.items[1])
+			if (resources && resources.items) {
+				console.log(resources, resources.items[1])
 				return resources.items[1].tags
 			}
 		})
 		.then((tags) => {
 			console.log(tags)
-			var result = Object.keys(tags).map(function (key) {
-				return tags[key];
-			});
-			return result
+			if (tags && typeof tags === 'object') {
+
+				var result = Object.keys(tags).map(function (key) {
+					return tags[key];
+				});
+				return result
+			}
 		})
 		.then((tagsArray) => {
 			var result = tagsArray.map(function (obj) {
 				return {
-					"name"	: obj["n"],
+					"name": obj["n"],
 					"tag_id": obj["c"],
-					"bus"		: obj["jp"]["bus"],
+					"bus": obj["jp"]["bus"],
 					"bus_id": obj["jp"]["bus_id"],
-					"pnid"	: obj["jp"]["pnid"],
-					"id" 		: obj["id"]
+					"pnid": obj["jp"]["pnid"],
+					"id": obj["id"]
 				}
 			})
 			return result
@@ -71,26 +74,26 @@ function update_data() {
 				}).then((parentData) => {
 					if (parentData.length > 0) {
 						parentData = parentData[0];
-						console.log(Object.keys(parentData.children ,element.tag_id ));
+						console.log(Object.keys(parentData.children, element.tag_id));
 						if (!Object.keys(parentData.children).includes(element.tag_id)) {
 							parentData.children[element.tag_id] = {
-								"bus"			: element.bus,
-								"bus_id"	: element.bus_id,
-								"name"		: element.name,
-								"id"  		: element.id,
-								"source"	: conf.source
+								"bus": element.bus,
+								"bus_id": element.bus_id,
+								"name": element.name,
+								"id": element.id,
+								"source": conf.source
 							}
 							rp({
-									uri: `http://localhost:3000/api/parents`,
-									method: "PUT",
-									header: {
-										"Content-Type": "application/json",
-										"Accept": "application/json"
-									},
-									body: parentData,
-									json: true
-								})
-								.then((success) => {}).catch((err) => {})
+								uri: `http://localhost:3000/api/parents`,
+								method: "PUT",
+								header: {
+									"Content-Type": "application/json",
+									"Accept": "application/json"
+								},
+								body: parentData,
+								json: true
+							})
+								.then((success) => { }).catch((err) => { })
 						}
 					}
 				}).catch((err) => console.log(err))
