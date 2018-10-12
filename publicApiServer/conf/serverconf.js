b63c8ec4711623fce2d99fa6904e15c4f06f7a1b
ezamlee <a.m.essam1992@gmail.com>
2018-10-12 15:21:41 +0200

    update gitignore

---
+++ publicApiServer/conf/serverconf.js
@@ -0,0 +1,8 @@
+module.exports = {
+	"token" 		 : "0e31585320d29e3db8ca8cbeab99ed5f71EF3260BD576810ADDBF2E282F94A8CE0050E86",
+	"secretWord" : "mysecretword",
+	"source"     : "15766062",
+	"mailPass"   : "P@$$w0rd123",
+	"mailUser"   : "schooltrackingsystems@gmail.com",
+	"cycle"			 : "dev"
+}
